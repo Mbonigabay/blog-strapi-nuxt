66972b12b469e4efe1ca2ca777565e8b059e4b59
Yusuf Mbonigaba <mbonigabay@gmail.com>
2022-08-28 14:39:09 +0200

    first commit

---
+++ blog-nuxt/pages/[[slug]].vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <h1>{{post.data[0].attributes.Title}}</h1>
+    </main>
+</template>
+<script setup>
+const route = useRoute()
+
+const { data } = await useFetch(`http://localhost:1337/api/posts?Slug=${route.params.slug},populate=user`);
+const post = data.value;
+console.log(route.params.slug)
+console.log(post.data)
+</script>
---
+++ blog-nuxt/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <div v-for="(post, index) in posts" :key="index">
+      <h2>{{post.attributes.Title}}</h2>
+      <p>{{post.attributes.user.data.attributes.username}}</p>
+    </div>
+  </main>
+</template>
+<script setup>
+const { data } = await useFetch("http://localhost:1337/api/posts?populate=user");
+const posts = data.value.data;
+console.log(posts);
+</script>
